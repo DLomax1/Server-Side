daf73886c93337045ff2dd4edb7fe514d4f544a2
DLomax1 <99465725+DLomax1@users.noreply.github.com>
2023-09-18 23:52:08 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Review_Lab;
+
+namespace lab2
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<VideoGame>> gamesByPlatform = new Dictionary<string, List<VideoGame>>();
+            string filePath = "D:\\Fall 23\\Server side\\Labs\\Lab2\\videogames.csv";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] array = sr.ReadLine().Split(',');
+
+                        VideoGame newGame = new VideoGame
+                        {
+                            Name = array[0],
+                            Platform = array[1],
+                            year = array[2],
+                            genre = array[3],
+                            publisher = array[4],
+                            NaSales = array[5].Trim(),
+                            EuSales = array[6],
+                            JpSales = array[7].Trim(),
+                            OtherSales = array[8],
+                            GlobalSales = array[9]
+                        };
+
+                        if (gamesByPlatform.ContainsKey(newGame.Platform))
+                        {
+                            gamesByPlatform[newGame.Platform].Add(newGame);
+                        }
+                        else
+                        {
+                            gamesByPlatform[newGame.Platform] = new List<VideoGame> { newGame };
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Func<string, List<VideoGame>> getTop5Games = platform =>
+            {
+                if (gamesByPlatform.TryGetValue(platform, out var games))
+                {
+                    return games.OrderByDescending(g => g.GlobalSales).Take(5).ToList();
+                }
+                return new List<VideoGame>();
+            };
+
+            foreach (var platform in gamesByPlatform.Keys)
+            {
+                Console.WriteLine($"Top 5 games for {platform}:");
+                var top5Games = getTop5Games(platform);
+
+                foreach (var game in top5Games)
+                {
+                    Console.WriteLine($"{game.Name} - Global Sales: {game.GlobalSales}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
---
+++ VideoGame.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Lab
+{
+    public class VideoGame : IComparable<VideoGame>
+    {
+       public string Name;
+        public string Platform;
+        public string year;
+        public string genre;
+        public string publisher;
+        public  string NaSales;
+        public string EuSales;
+        public string JpSales;
+        public string OtherSales;
+        public string GlobalSales;
+        public VideoGame videoGame;
+
+       public VideoGame(string name, string platform, string year, string genre, string publisher, string naSales, string euSales, string jpSales, string otherSales, string globalSales)
+        {
+            Name = name;
+            Platform = platform;
+            this.year = year;
+            this.genre = genre;
+            this.publisher = publisher;
+            NaSales = naSales;
+            EuSales = euSales;
+            JpSales = jpSales;
+            OtherSales = otherSales;
+            GlobalSales = globalSales;
+        }
+
+       public VideoGame()
+        {
+            Name = string.Empty;
+            Platform = string.Empty;
+            year= string.Empty;
+            genre = "Strategy";
+            publisher = string.Empty;
+            NaSales = string.Empty;
+            EuSales = string.Empty;
+            JpSales = string.Empty;
+            OtherSales = string.Empty;
+            GlobalSales = string.Empty;
+
+        }
+
+       public VideoGame(VideoGame existingVideoGame)
+        {
+
+            existingVideoGame.Name = Name;
+            existingVideoGame.Platform = Platform;
+            existingVideoGame.year = year;
+            existingVideoGame.genre = genre;
+            existingVideoGame.publisher = publisher;
+            existingVideoGame.NaSales = NaSales;
+            existingVideoGame.EuSales = EuSales;
+            existingVideoGame.JpSales = JpSales;
+            existingVideoGame.OtherSales = OtherSales;
+            existingVideoGame.GlobalSales = GlobalSales;
+        }
+
+        public override string ToString()
+        {
+            string VideoGameString = string.Empty;
+
+            VideoGameString += $"Name: {Name}\n";
+            VideoGameString += $"Platform: {Platform}\n";
+            VideoGameString += $"Year: {year}\n";
+            VideoGameString += $"Genre: {genre}\n";
+            VideoGameString += $"Publisher: {publisher}\n";
+            VideoGameString += $"Na-Sales: {NaSales}\n";
+            VideoGameString += $"Eu-Sales: {EuSales}\n";
+            VideoGameString += $"Jp-Sales: {JpSales}\n";
+            VideoGameString += $"Other Sales: {OtherSales}\n";
+            VideoGameString += $"Global Sales: {GlobalSales}\n";
+
+            return VideoGameString;
+        }
+
+        public int CompareTo(VideoGame other)
+        {
+            if (other == null)
+                return 1;
+
+            // Compare titles in ascending order
+            return string.Compare(this.Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
